3775a053959ddf207bbfc9c34fda41fedc905a15
Xrenore3 <romanchaus519@gmail.com>
2021-03-10 17:28:02 +0200

    delete

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('hello')
-console.log('change on roman')
-console.log('change on roman second')
-console.log('change on roman third')
+
 
 
 
